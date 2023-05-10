6affc3aeb771afcec0f359acbd4c6c1a97272a59
Jeon-HS4 <jin0004@tukorea.ac.kr>
2023-05-11 02:46:17 +0900

    url model Update

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-
-from pybo import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pybo/', views.index),
+    path('pybo/', include('pybo.urls')),
 ]
--- pybo/models.py
+++ pybo/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 class Question(models.Model):
-    subject = models.CharField(max_length=200)
+    # 제목, 작성자, 내용, 카테고리, 첨부파일, 날짜
+    title = models.CharField()
+    author = models.CharField()
     content = models.TextField()
-    create_date = models.DateTimeField()
+    category = models.CharField()
+    addFile = models.FileField()
+    write_date = models.DateTimeField()
 
-
+'''
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
@@ -13,3 +17,4 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.subject
+'''
