542bb1a818a2af0e24a9dd547fd900be07cde7fc
Jeon-HS4 <jin0004@tukorea.ac.kr>
2023-05-13 22:21:42 +0900

    기본 Setting

--- pybo/models.py
+++ pybo/models.py
@@ -1,20 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Question(models.Model):
     # 제목, 작성자, 내용, 카테고리, 첨부파일, 날짜
-    title = models.CharField()
-    author = models.CharField()
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default="user")
+    #author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     content = models.TextField()
-    category = models.CharField()
-    addFile = models.FileField()
+    catego = models.CharField(max_length=50, default="diary")
+    addFile = models.FileField(upload_to='uploads/', null=True, blank=True)
     write_date = models.DateTimeField()
-
-'''
-class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    content = models.TextField()
-    create_date = models.DateTimeField()
 
     def __str__(self):
         return self.subject
-'''
