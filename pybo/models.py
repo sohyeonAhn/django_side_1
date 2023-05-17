137c27d920a630b3bfd8b0c7a30ee59067220831
sohyeon910 <dksthgus777@naver.com>
2023-05-18 03:11:45 +0900

    first commit test_diary

--- pybo/models.py
+++ pybo/models.py
@@ -1,15 +1,16 @@
+"""Model class to define the database table for entries"""
+
 from django.db import models
+from django.utils import timezone
 
-class Question(models.Model):
-    subject = models.CharField(max_length=200)
+class Entry(models.Model):
+    title = models.CharField(max_length=200)
     content = models.TextField()
-    create_date = models.DateTimeField()
-
-
-class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    content = models.TextField()
-    create_date = models.DateTimeField()
+    date_created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.subject
+        return self.title
+
+    class Meta:
+        """define plural for the class"""
+        verbose_name_plural = "Pybo"
--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,49 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from .models import Entry
 
-def index(request):
-    return HttpResponse("안녕하세요 환영합니다.")
+class LockedView(LoginRequiredMixin):
+    login_url = "admin:login"
+
+class EntryListView(LockedView, ListView):
+    model = Entry
+    queryset = Entry.objects.all().order_by("-date_created")
+
+
+class EntryDetailView(LockedView, DetailView):
+    model = Entry
+
+
+class EntryCreateView(LockedView, SuccessMessageMixin, CreateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_url = reverse_lazy("entry-list")
+    success_message = "Your new entry was created!"
+
+
+class EntryUpdateView(LockedView, SuccessMessageMixin, UpdateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_message = "Your entry was updated!"
+
+    def get_success_url(self):
+        return reverse_lazy("entry-detail", kwargs={"pk": self.object.pk})
+
+
+class EntryDeleteView(LockedView, DeleteView):
+    model = Entry
+    success_url = reverse_lazy("entry-list")
+    success_message = "Your entry was deleted!"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
